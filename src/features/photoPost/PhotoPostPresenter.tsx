e11113dff184b0f1f5a546ade6e375a24511d685
ひろ <koki.hiro0723@gmail.com>
2024-08-14 01:12:32 +0900

    投稿画面のUI作成

--- src/features/photoPost/PhotoPostPresenter.tsx
+++ src/features/photoPost/PhotoPostPresenter.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -42,35 +43,40 @@ const PhotoPostPresenter = ({ form, onSubmit }: PhotoPostContainerProps) => {
 
   return (
     <div className="flex flex-col items-center">
-      {/* <Header /> */}
+      <Header />
 
-      <p>写真の情報</p>
+      <p className="text-lg font-bold p-2">写真の投稿</p>
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-4/5 mx-auto"
+        >
           <FormField
             control={form.control}
             name="image"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>写真</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="写真"
-                    id="picture"
-                    type="file"
-                    accept="image/*"
-                    capture="environment"
-                    // fieldを使用してreact-hook-formに接続
-                    onChange={(e) => {
-                      const file = e.target.files?.[0];
-                      if (file) {
-                        field.onChange(file); // field.onChangeを使用してファイルを設定
-                      }
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
+                <div className="bg-[#E3D8C6] p-2 rounded-lg">
+                  <FormLabel className="text-lg font-bold">写真</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="写真"
+                      id="picture"
+                      type="file"
+                      accept="image/*"
+                      capture="environment"
+                      // fieldを使用してreact-hook-formに接続
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        if (file) {
+                          field.onChange(file); // field.onChangeを使用してファイルを設定
+                        }
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </div>
               </FormItem>
             )}
           />
@@ -79,12 +85,18 @@ const PhotoPostPresenter = ({ form, onSubmit }: PhotoPostContainerProps) => {
             control={form.control}
             name="title"
             render={({ field }) => (
-              <FormItem>
-                <FormLabel>タイトル</FormLabel>
-                <FormControl>
-                  <Input placeholder="タイトル" {...field} />
-                </FormControl>
-                <FormMessage />
+              <FormItem className="w-full">
+                <div className="bg-[#E3D8C6] p-2 rounded-lg">
+                  <FormLabel className="text-lg font-bold">タイトル</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="タイトル"
+                      {...field}
+                      className="text-lg font-bold"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </div>
               </FormItem>
             )}
           />
@@ -94,64 +106,69 @@ const PhotoPostPresenter = ({ form, onSubmit }: PhotoPostContainerProps) => {
             name="location"
             render={({ field }) => (
               <FormItem className="flex flex-col">
-                <FormLabel>場所</FormLabel>
-                <Popover open={open} onOpenChange={setOpen}>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      aria-expanded={open}
-                      className="w-[200px] justify-between"
-                    >
-                      {value
-                        ? LOCATIONS.find((location) => location.value === value)
-                            ?.label
-                        : "場所を選択..."}
-                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-[200px] p-0">
-                    <Command>
-                      <CommandInput placeholder="場所を選択..." />
-                      <CommandList>
-                        <CommandEmpty>場所が見つかりません。</CommandEmpty>
-                        <CommandGroup>
-                          {LOCATIONS.map((location) => (
-                            <CommandItem
-                              key={location.value}
-                              value={location.value}
-                              onSelect={(currentValue) => {
-                                setValue(
-                                  currentValue === value ? "" : currentValue
-                                );
-                                field.onChange(
-                                  currentValue === value ? "" : currentValue
-                                );
-                                setOpen(false);
-                              }}
-                            >
-                              <Check
-                                className={`mr-2 h-4 w-4 ${
-                                  value === location.value
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                }`}
-                              />
-                              {location.label}
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </CommandList>
-                    </Command>
-                  </PopoverContent>
-                </Popover>
+                <div className="bg-[#E3D8C6] p-2 rounded-lg">
+                  <FormLabel className="text-lg font-bold">場所</FormLabel>
+                  <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={open}
+                        className="w-full justify-between text-lg font-bold"
+                      >
+                        {value
+                          ? LOCATIONS.find(
+                              (location) => location.value === value
+                            )?.label
+                          : "場所を選択..."}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                      <Command>
+                        <CommandInput placeholder="場所を選択..." />
+                        <CommandList>
+                          <CommandEmpty>場所が見つかりません。</CommandEmpty>
+                          <CommandGroup>
+                            {LOCATIONS.map((location) => (
+                              <CommandItem
+                                key={location.value}
+                                value={location.value}
+                                onSelect={(currentValue) => {
+                                  setValue(
+                                    currentValue === value ? "" : currentValue
+                                  );
+                                  field.onChange(
+                                    currentValue === value ? "" : currentValue
+                                  );
+                                  setOpen(false);
+                                }}
+                              >
+                                <Check
+                                  className={`mr-2 h-4 w-4 ${
+                                    value === location.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  }`}
+                                />
+                                {location.label}
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                </div>
                 <FormMessage />
               </FormItem>
             )}
           />
 
           <div className="flex justify-center">
-            <Button type="submit">投稿</Button>
+            <Button type="submit" className="w-4/5 h-12 text-lg bg-red">
+              投稿
+            </Button>
           </div>
         </form>
       </Form>
