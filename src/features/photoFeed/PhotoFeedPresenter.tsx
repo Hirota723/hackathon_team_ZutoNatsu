7e5166571a3bb569fe968f43704e2777831cef76
hetaredaimaou <hanadekayu@icloud.com>
2024-08-13 16:10:18 +0900

    ヘッダの追加

--- src/features/photoFeed/PhotoFeedPresenter.tsx
+++ src/features/photoFeed/PhotoFeedPresenter.tsx
@@ -1,11 +1,12 @@
 import Footer from "@/components/Footer";
+import Header from "@/components/Header"
 import React from "react";
 
 const PhotoFeedPresenter = () => {
   return (
     <div>
+      <Header />
       <p>PhotoFeedPresenter</p>
-
       <Footer />
     </div>
   );
