bb2fded1eedfcb368e93c4759ac1ed80e6610af2
Yamazaki Hyuga <hyugayamazaki@icloud.com>
2024-08-13 15:37:15 +0900

    フッターのUI作成

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,26 +3,34 @@ import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-300 flex flex-col items-center py-4">
-      <ul className="flex">
-        <li>
+    <div className="fixed bottom-0 left-0 right-0 bg-teal-800 flex justify-around items-center py-4">
+      <ul className="flex justify-around w-full">
+        <li className="flex flex-col items-center">
           <Button variant="link">
-            <Link href={"/home"}>ホーム</Link>
+            <Link href="/home">
+              <img src="/images/home_icon.png" alt="ホーム" className="w-6 h-6" />
+            </Link>
           </Button>
         </li>
-        <li>
+        <li className="flex flex-col items-center">
           <Button variant="link">
-            <Link href={"/post"}>投稿</Link>
+            <Link href="/post">
+              <img src="/images/post_icon.png" alt="投稿" className="w-6 h-6" />
+            </Link>
           </Button>
         </li>
-        <li>
+        <li className="flex flex-col items-center">
           <Button variant="link">
-            <Link href={"/ranking"}>ランキング</Link>
+            <Link href="/ranking">
+              <img src="/images/ranking_icon.png" alt="ランキング" className="w-6 h-6" />
+            </Link>
           </Button>
         </li>
-        <li>
+        <li className="flex flex-col items-center">
           <Button variant="link">
-            <Link href={"/my-photos"}>マイページ</Link>
+            <Link href="/my-photos">
+              <img src="/images/my_photos_icon.png" alt="マイページ" className="w-6 h-6" />
+            </Link>
           </Button>
         </li>
       </ul>
